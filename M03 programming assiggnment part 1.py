a9160ea7abf6df40f133f75dc369d72f7104699e
Ilyaskhamliche <113136909+Ilyaskhamliche@users.noreply.github.com>
2023-11-10 12:24:35 -0500

    Update M03 programming assignment part 1.py

--- M03 programming assiggnment part 1.py
+++ M03 programming assiggnment part 1.py
@@ -1,36 +1,12 @@
-years_list = [year for year in range(1995, 2000)]
-third_birthday_year = years_list[2]
-oldest_year = years_list[-1]
+years_list = [year for year in range(2003,2009)]
+third_birthday_year = years_list[3]
+oldest_year = years_list[5]
 things = ["mozzarella", "cinderella", "salmonella"]
 things[1] = things[1].capitalize()
 things[0] = things[0].upper()
 things.remove("salmonella")
-surprise = ["Groucho", "Chico", "Harpo"]
-surprise[-1] = surprise[-1].lower()[::-1].capitalize()
-even = [num for num in range(10) if num % 2 == 0]
-start1 = ["fee", "fie", "foe"]
-rhymes = [
-    ("flop", "get a mop"),
-    ("fope", "turn the rope"),
-    ("fa", "get your ma"),
-    ("fudge", "call the judge"),
-    ("fat", "pet the cat"),
-    ("fog", "walk the dog"),
-    ("fun", "say we're done"),
-]
-start2 = "Someone better"
 
-for first, second in rhymes:
-    # First line
-    print(" ".join(word.capitalize() + "!" for word in start1) + " " + first.capitalize() + "!")
-    
-    # Second line
-    print(start2 + " " + second + ".")
-
-# Print results
 print("7.1:", years_list)
 print("7.2:", third_birthday_year)
 print("7.3:", oldest_year)
 print("7.4:", things)
-print("7.8:", surprise)
-print("7.10:", even)
